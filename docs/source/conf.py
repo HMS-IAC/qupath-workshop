af528c178907d808dfab4e9e75ac744343c1e76b
Antoine A. Ruzette <antoine.ruzette@gmail.com>
2024-09-13 11:38:56 -0400

    change theme to pydata to support multiversion

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,53 +1,47 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'QuPath Workshop at Harvard Medical School'
-copyright = '2024, Antoine A. Ruzette, Simon F. Nørrelykke. Licensed under CC-BY 4.0 International.'
+copyright = '2024, Antoine A. Ruzette, Simon F. Nørrelykke'
 author = 'Antoine A. Ruzette, Simon F. Nørrelykke'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
     "sphinx_copybutton",
-    "sphinx_multiversion",
+    "sphinx_multiversion",  # For versioning
 ]
-autodoc_typehints = "both"
-napoleon_use_ivar = True
 
-templates_path = ['_templates']
+templates_path = ['_templates']  # Make sure the templates directory is included
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'pydata_sphinx_theme'
 
-html_theme = 'furo'
-html_static_path = ['_static']
-html_logo = "_static/iac-hms-logo.png"  # Specify the path to your logo file
-html_title = 'QuPath Workshop at Harvard Medical School'
-
-# Furo theme options
+# PyData theme options
 html_theme_options = {
-    "sidebar_hide_name": True,
-    # No 'version_dropdown' option here
+    'logo': {
+        'text': 'QuPath Workshop',  # Use text if no image logo is available
+    },
+    'navbar_end': ['navbar-icon-links'],  # Keep this simple since we use the custom version list
+    'navbar_persistent': ['search-field'],  # Keep the search field persistent
 }
 
-# -- Options for sphinx-multiversion -----------------------------------------
-# Whitelist branch names in the format YYYY_MM_DD
-smv_branch_whitelist = r'^\d{4}_\d{2}_\d{2}$'
+# Sidebar settings for navigation and custom versioning
+html_sidebars = {
+    "**": ["sidebar-nav-bs", "search-field", "versions.html"],  # Include the custom version list
+}
 
-# (Optional) Exclude all other branches if needed
-smv_branch_exclude = r'^.*$'
-smv_tag_whitelist = r'^$'
-# Exclude the 'main' branch explicitly
-smv_branch_exclude = r'^main$'
+html_static_path = ['_static']
+html_logo = "_static/iac-hms-logo.png"
+html_title = 'QuPath Workshop at Harvard Medical School'
+
+# -- Options for sphinx-multiversion -----------------------------------------
+smv_branch_whitelist = r'^\d{4}_\d{2}_\d{2}$'  # Match branches with date format
+smv_branch_exclude = r'^main$'  # Exclude the 'main' branch
+smv_tag_whitelist = r'^$'  # Exclude all tags
+
 # Set the HTML base URL for correct linking
 html_baseurl = 'https://hms-iac.github.io/qupath-workshop/'
